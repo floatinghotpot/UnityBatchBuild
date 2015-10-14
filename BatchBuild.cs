60a8c4594e8312180ff83effc88145e7fa9c99db
Liming Xie <liming.xie@gmail.com>
2015-10-14 13:28:18 +0800

    start proj

---
+++ BatchBuild.cs
@@ -0,0 +1,26 @@
+﻿
+#if UNITY_EDITOR
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class BatchBuild : MonoBehaviour {
+	[MenuItem("Tools/Build/Test")]
+	public static void Test() {
+		Debug.Log ("BatchBuildTest");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+#endif
